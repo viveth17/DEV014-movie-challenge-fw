31768081d0284fc38c1ddd4e5ce1347eb6111eb8
viveth17 <95vizcarra@gmail.com>
2024-05-31 12:40:28 -0700

    fix(MovieCard): :bug: restore commented-out functional code in APP

--- movie-next/src/App.tsx
+++ movie-next/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import { MovieCard } from './components/MovieCard'
 import Movie from './models/Movie'
