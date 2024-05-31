3f7b3f276af14a94aa46fbe0aa56ea8ad814df09
viveth17 <95vizcarra@gmail.com>
2024-05-31 12:37:49 -0700

    fix: :bug: restore commented-out functional code in APP

--- movie-next/src/App.tsx
+++ movie-next/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <>
-      {/* <div>
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -41,7 +41,7 @@ function App() {
       </div>
       <p title="read-the-docs" className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p>
       <MovieCard movie={exampleMovie} />
     </>
   )
