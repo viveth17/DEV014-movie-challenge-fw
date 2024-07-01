7fcf5b562c89602b87e393e3c4eb4f07510ab940
viveth17 <95vizcarra@gmail.com>
2024-07-01 11:39:47 -0700

    feat(ListOptions): :sparkles: Create ListOptions component

---
+++ movie-next/src/components/ListOptions.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import styles from '../styles/ListOptions.module.css';
+
+interface Option {
+    value: string;
+    label: string;
+}
+
+interface ListOptionsProps { // para almacenar las opciones a listar 
+    options: Option[]; //almacena las opciones que se listaran en el <select>
+    selectedOption: Option | null; // almacen la opcion seleccionada actualmente o null si no hay ninguna seleccionada
+    onChange: (option: Option | null) => void; //funcion que se ejecuta cuando se selecciona una opcion del <select>
+    onClear: () => void; // funcion que se ejecuta cuando el usuario borra la seleccion actual 
+}
+
+export const ListOptions: React.FC<ListOptionsProps> = ({ options, selectedOption, onChange, onClear }) => {
+    return (
+        <div className={styles.container}>
+            {/* Renderizar las opciones  */}
+            <div className={styles.selectContainer}>
+                <select
+                    value={selectedOption ? selectedOption.value : ''}
+                    onChange={(e) => {
+                        const selectedValue = e.target.value;
+                        const option = options.find(opt => opt.value === selectedValue) || null;
+                        onChange(option);
+                    }}
+                >
+                    <option value="">--Selecciona una opción--</option>
+                    {options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            {/* Mostrar la opción seleccionada si existe */}
+            {selectedOption && (
+                <div className={styles.selectedOption}>
+                    <span>Opción seleccionada: {selectedOption.label}: {selectedOption.value}</span>
+                    <button className={styles.clearButton} onClick={onClear}> Borrar selección</button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ListOptions;
