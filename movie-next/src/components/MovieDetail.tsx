663c346166928a3ce4081754316f240250ed5c6a
viveth17 <95vizcarra@gmail.com>
2024-07-04 14:07:20 -0700

    style(MovieDetail): :lipstick: add MovieDetail component with dynamic movie loading and styling using CSS modules

--- movie-next/src/components/MovieDetail.tsx
+++ movie-next/src/components/MovieDetail.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Movie from '../models/Movie';
 import { getMovieDetail } from '../services/movieService';
 import styles from '../styles/MovieDetail.module.css';
+import { FaArrowLeft, FaStar } from 'react-icons/fa'; //  icono de flecha y estrella
 
 const MovieDetail: React.FC= () => {
 
     const {id} = useParams<{id: string}>(); // para obtener el "id" de la url dinámicamente 
+    const navigate = useNavigate(); //Hook para la navegación
 
     const [movie, setMovie] = useState<Movie | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
@@ -36,31 +38,38 @@ const MovieDetail: React.FC= () => {
     const genres = movie.genres.join(", ");
 
     return (
-        <div className={styles['movie-detail']}>
-            <div className={styles['poster-container']}>
-                {movie.poster_path ? (
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster}`}
-                        alt={`${movie.title || "No title available"} poster`}
-                        className={styles['poster-image']}
-                    />
-                ) : (
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster}`}
-                        alt="No poster available"
-                        className={styles['poster-image']}
-                    />
-                )}
+        <><div className='back-list'>
+            <div className={styles['back-button']} onClick={() => navigate(-1)}>
+                <FaArrowLeft className={styles['back-icon']} /> Regresar al listado
             </div>
-            <div className={styles['details-container']}>
-                {/* <h2 className={styles['movie-title']}>{movie.title || "No title available"}</h2> */}
-                <h2 className={styles['original-title']}>{movie.original_title || "Título original no disponible"}({releaseYear})</h2>
-                <p className={styles['synopsis']}>Sinópsis: {movie.overview || "Sinopsis no disponible"}</p>
-                <p className={styles['rating']}>Calificación: {movie.rating}/10</p>
-                {/* <p className={styles['release-year']}>Año de lanzamiento: {releaseYear}</p> */}
-                <p className={styles['genres']}>Géneros: {genres}</p>
-            </div>
-        </div>
+        </div><div className={styles['movie-detail']}>
+                {/* <button className={styles['back-button']} onClick={() => navigate(-1)}>
+        <FaArrowLeft className={styles['back-icon']} /> Regresar al listado
+    </button> */}
+                <div className={styles['poster-container']}>
+                    {movie.poster_path ? (
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${movie.poster}`}
+                            alt={`${movie.title || "No title available"} poster`}
+                            className={styles['poster-image']} />
+                    ) : (
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${movie.poster}`}
+                            alt="No poster available"
+                            className={styles['poster-image']} />
+                    )}
+                </div>
+                <div className={styles['details-container']}>
+                    {/* <h2 className={styles['movie-title']}>{movie.title || "No title available"}</h2> */}
+                    <h2 className={styles['original-title']}>{movie.original_title || "Título original no disponible"}({releaseYear}) </h2>
+                    <p className={styles['rating']}><FaStar className={styles['star-icon']} />{movie.rating}/10</p>
+                    <p className={styles['genres']}>Géneros: {genres}</p>
+                    <p className={styles['synopsis']}>Sinópsis: {movie.overview || "Sinopsis no disponible"}</p>
+                    {/* <p className={styles['rating']}><FaStar className={styles['star-icon']} />{movie.rating}/10</p> */}
+                    {/* <p className={styles['release-year']}>Año de lanzamiento: {releaseYear}</p> */}
+                    {/* <p className={styles['genres']}>Géneros: {genres}</p> */}
+                </div>
+            </div></>
     );
 };
 
