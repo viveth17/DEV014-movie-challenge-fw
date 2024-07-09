e3ce73e43e9d6a9a88cd0dd32d53e696c01bb79d
viveth17 <95vizcarra@gmail.com>
2024-07-08 18:13:41 -0700

    test(APIService): :white_check_mark: adjust unit tests for getMovies method

--- movie-next/src/services/APIService.spec.ts
+++ movie-next/src/services/APIService.spec.ts
@@ -6,11 +6,11 @@ import fetchMock from 'jest-fetch-mock';
 
 fetchMock.enableMocks();
 
- 
+
 describe('getMovies', () => {
 
   //Limpiar el mock antes de cada prueba (Restauración de los mocks, un test no afecte a los demas test)
- 
+
   beforeEach(() => {
     fetchMock.resetMocks();
   });
@@ -21,17 +21,20 @@ describe('getMovies', () => {
 
     const mockMovies = {
 
-          page: 1, 
-          total_pages: 10,
-          results: [
-            {
+      page: 1,
+      total_pages: 10,
+      results: [
+        {
           genre_ids: [878, 28, 12],
           poster_path: "/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg",
           release_date: "2024-03-27",
           title: "Godzilla x Kong: The New Empire",
           vote_average: 7.261,
-          backdrop_path : "/xRd1eJIDe7JHO5u4gtEYwGn5wtf.jpg"
-          
+          backdrop_path: "/some_backdrop_path.jpg",
+          id: 12345,
+          original_title: "Godzilla vs Kong Original Title",
+          overview: "This is an overview."
+
         }
       ]
     };
@@ -42,26 +45,30 @@ describe('getMovies', () => {
     ]);
 
     const responseMovies = [
-        {
-          genres: ["Science Fiction", "Action", "Adventure"],
-          poster: "/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg",
-          releaseYear: "2024-03-27",
-          title: "Godzilla x Kong: The New Empire",
-          rating: 7.261,
-          backdrop_path : "/xRd1eJIDe7JHO5u4gtEYwGn5wtf.jpg"
-        }
-      ];
+      {
+        genres: ["Science Fiction", "Action", "Adventure"],
+        poster: "/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg",
+        releaseYear: "2024-03-27",
+        title: "Godzilla x Kong: The New Empire",
+        rating: 7.261,
+        backdrop_path: "/some_backdrop_path.jpg",
+        id: 12345, 
+        original_title: "Godzilla vs Kong Original Title", 
+        overview: "This is an overview.", 
+        poster_path: "/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg"
+      }
+    ];
     fetchMock.mockResponseOnce(JSON.stringify(mockMovies));
 
     return getMovies({ filters: { page: 1 } }, mockGenresMap)
-    .then( response =>{
-      expect(fetchMock).toHaveBeenCalledWith(
-        `https://api.themoviedb.org/3/discover/movie?api_key=mock_api_key&page=${1}`
-      );
-      expect(response.movies).toEqual(responseMovies);
-      expect(response.metaData.pagination.currentPage).toBe(1);
-      expect(response.metaData.pagination.totalPages).toBe(10);
-    });
+      .then(response => {
+        expect(fetchMock).toHaveBeenCalledWith(
+          `https://api.themoviedb.org/3/discover/movie?api_key=mock_api_key&page=${1}`
+        );
+        expect(response.movies).toEqual(responseMovies);
+        expect(response.metaData.pagination.currentPage).toBe(1);
+        expect(response.metaData.pagination.totalPages).toBe(10);
+      });
   });
   it('Debería devolver una lista de películas para la segunda página', () => {
     const mockMovies = {
@@ -74,7 +81,11 @@ describe('getMovies', () => {
           release_date: "2024-03-27",
           title: "Another Movie",
           vote_average: 6.5,
-          backdrop_path: "/another_backdrop.jpg"
+          backdrop_path: "/another_backdrop.jpg",
+          id: undefined, // Simula un id indefinido
+          original_title: undefined, // Simula un original_title indefinido
+          overview: undefined // Simula un overview indefinido
+
         }
       ]
     };
@@ -91,7 +102,12 @@ describe('getMovies', () => {
         releaseYear: "2024-03-27",
         title: "Another Movie",
         rating: 6.5,
-        backdrop_path: "/another_backdrop.jpg"
+        backdrop_path: "/another_backdrop.jpg",
+        id: undefined,
+        original_title: undefined,
+        overview: undefined,
+        poster_path: "/another_poster.jpg"
+
       }
     ];
 
@@ -114,9 +130,9 @@ describe('getMovies', () => {
 
 
     return getMovies({ filters: { page: 1 } }, new Map())
-    .catch(error => {
-      expect(error).toEqual(new Error('Failed to fetch'));
-    });
+      .catch(error => {
+        expect(error).toEqual(new Error('Failed to fetch'));
+      });
   });
 
   it('debería lanzar un error si la solicitud no es exitosa', () => {
@@ -124,10 +140,10 @@ describe('getMovies', () => {
     fetchMock.mockResponseOnce(JSON.stringify({}), { status: 404, statusText: 'Not Found' });
 
 
-    return getMovies({ filters: { page: 1 } } , new Map())
-    .catch(error => {
-      expect(error).toEqual(new Error ('Network response was not ok'));
-    });
+    return getMovies({ filters: { page: 1 } }, new Map())
+      .catch(error => {
+        expect(error).toEqual(new Error('Network response was not ok'));
+      });
   });
 
   it('Debería manejar películas sin géneros', () => {
@@ -141,7 +157,10 @@ describe('getMovies', () => {
           release_date: "2024-03-27",
           title: "No Genre Movie",
           vote_average: 5.5,
-          backdrop_path: "/no_genre_backdrop.jpg"
+          backdrop_path: "/no_genre_backdrop.jpg",
+          id: 1, // Asegúrate de incluir id
+          original_title: "No Genre Movie Original Title", // Asegúrate de incluir original_title
+          overview: "This is an overview." // Asegúrate de incluir overview
         }
       ]
     };
@@ -153,7 +172,12 @@ describe('getMovies', () => {
         releaseYear: "2024-03-27",
         title: "No Genre Movie",
         rating: 5.5,
-        backdrop_path: "/no_genre_backdrop.jpg"
+        backdrop_path: "/no_genre_backdrop.jpg",
+        id: 1, // Incluye id
+        original_title: "No Genre Movie Original Title", // Incluye original_title
+        overview: "This is an overview.", // Incluye overview
+        poster_path: "/no_genre_poster.jpg" // Incluye poster_path
+
       }
     ];
 
@@ -178,7 +202,10 @@ describe('getMovies', () => {
           release_date: null,
           title: null,
           vote_average: null,
-          backdrop_path: null
+          backdrop_path: null,
+          id: undefined, // simula un id indefinido
+          original_title: undefined, //simula un original_title indefinido
+          overview: undefined // simula un overview indefinido
         }
       ]
     };
@@ -194,7 +221,11 @@ describe('getMovies', () => {
         releaseYear: null,
         title: null,
         rating: null,
-        backdrop_path: null
+        backdrop_path: null,
+        id: undefined,
+        original_title: undefined,
+        overview: undefined,
+        poster_path: null
       }
     ];
 
@@ -202,6 +233,160 @@ describe('getMovies', () => {
 
     return getMovies({ filters: { page: 1 } }, mockGenresMap)
       .then(response => {
+        expect(fetchMock).toHaveBeenCalledWith(
+          `https://api.themoviedb.org/3/discover/movie?api_key=mock_api_key&page=${1}`
+        );
+        expect(response.movies).toEqual(responseMovies);
+        expect(response.metaData.pagination.currentPage).toBe(1);
+        expect(response.metaData.pagination.totalPages).toBe(10);
+      });
+  });
+  it('Debería devolver películas filtradas por género y ordenadas por popularidad descendente', () => {
+    const mockMovies = {
+      page: 1,
+      total_pages: 10,
+      results: [
+        {
+          genre_ids: [28],
+          poster_path: "/action_popular_movie.jpg",
+          release_date: "2024-03-27",
+          title: "Action Popular Movie",
+          vote_average: 8.0,
+          backdrop_path: "/action_popular_backdrop.jpg",
+          id: undefined, //simula un id indefinido
+          original_title: undefined, //simula un original_title indefinido
+          overview: undefined // simula un overview indefinido
+        }
+      ]
+    };
+
+    const mockGenresMap: Map<number, string> = new Map<number, string>([
+      [28, 'Action']
+    ]);
+
+    const responseMovies = [
+      {
+        genres: ["Action"],
+        poster: "/action_popular_movie.jpg",
+        releaseYear: "2024-03-27",
+        title: "Action Popular Movie",
+        rating: 8.0,
+        backdrop_path: "/action_popular_backdrop.jpg",
+        id: undefined,
+        original_title: undefined,
+        overview: undefined,
+        poster_path: "/action_popular_movie.jpg"
+      }
+    ];
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockMovies));
+
+    return getMovies({ filters: { page: 1, genreId: 28, sortBy: 'popularity.desc' } }, mockGenresMap)
+      .then(response => {
+        expect(fetchMock).toHaveBeenCalledWith(
+          `https://api.themoviedb.org/3/discover/movie?api_key=mock_api_key&page=1&with_genres=28&sort_by=popularity.desc`
+        );
+        expect(response.movies).toEqual(responseMovies);
+        expect(response.metaData.pagination.currentPage).toBe(1);
+        expect(response.metaData.pagination.totalPages).toBe(10);
+      });
+  });
+  it('Debería devolver películas para una página específica', () => {
+    const mockMovies = {
+      page: 3,
+      total_pages: 10,
+      results: [
+        {
+          genre_ids: [28],
+          poster_path: "/page3_movie.jpg",
+          release_date: "2024-03-27",
+          title: "Page 3 Movie",
+          vote_average: 7.2,
+          backdrop_path: "/page3_backdrop.jpg",
+          id: undefined, // Simula un id indefinido
+          original_title: undefined, // Simula un original_title indefinido
+          overview: undefined // Simula un overview indefinido
+        }
+      ]
+    };
+
+    const mockGenresMap: Map<number, string> = new Map<number, string>([
+      [28, 'Action']
+    ]);
+
+    const responseMovies = [
+      {
+        genres: ["Action"],
+        poster: "/page3_movie.jpg",
+        releaseYear: "2024-03-27",
+        title: "Page 3 Movie",
+        rating: 7.2,
+        backdrop_path: "/page3_backdrop.jpg",
+        id: undefined,
+        original_title: undefined,
+        overview: undefined,
+        poster_path: "/page3_movie.jpg"
+      }
+    ];
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockMovies));
+
+    return getMovies({ filters: { page: 3 } }, mockGenresMap)
+      .then(response => {
+        expect(fetchMock).toHaveBeenCalledWith(
+          `https://api.themoviedb.org/3/discover/movie?api_key=mock_api_key&page=3`
+        );
+        expect(response.movies).toEqual(responseMovies);
+        expect(response.metaData.pagination.currentPage).toBe(3);
+        expect(response.metaData.pagination.totalPages).toBe(10);
+      });
+  });
+  it('Debería devolver películas filtradas por género y ordenadas por popularidad descendente', () => {
+    const mockMovies = {
+      page: 1,
+      total_pages: 10,
+      results: [
+        {
+          genre_ids: [28],
+          poster_path: "/action_popular_movie.jpg",
+          release_date: "2024-03-27",
+          title: "Action Popular Movie",
+          vote_average: 8.0,
+          backdrop_path: "/action_popular_backdrop.jpg",
+          id: undefined, // Simula un id indefinido
+          original_title: undefined, // Simula un original_title indefinido
+          overview: undefined // Simula un overview indefinido
+
+        }
+      ]
+    };
+
+    const mockGenresMap: Map<number, string> = new Map<number, string>([
+      [28, 'Action']
+    ]);
+
+    const responseMovies = [
+      {
+        genres: ["Action"],
+        poster: "/action_popular_movie.jpg",
+        releaseYear: "2024-03-27",
+        title: "Action Popular Movie",
+        rating: 8.0,
+        backdrop_path: "/action_popular_backdrop.jpg",
+        id: undefined,
+        original_title: undefined,
+        overview: undefined,
+        poster_path: "/action_popular_movie.jpg"
+      }
+    ];
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockMovies));
+
+    return getMovies({ filters: { page: 1, genreId: 28, sortBy: 'popularity.desc' } }, mockGenresMap)
+      .then(response => {
+        expect(fetchMock).toHaveBeenCalledWith(
+          `https://api.themoviedb.org/3/discover/movie?api_key=mock_api_key&page=1&with_genres=28&sort_by=popularity.desc`
+        );
         expect(response.movies).toEqual(responseMovies);
         expect(response.metaData.pagination.currentPage).toBe(1);
         expect(response.metaData.pagination.totalPages).toBe(10);
